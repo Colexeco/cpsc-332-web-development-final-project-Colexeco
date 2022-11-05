7946478992828dd454fa0ac4d1a0e7b82e8eaaaa
Colexeco <evan.shoemaker0@gmail.com>
2022-11-05 09:52:08 -0700

    deleted app.js, added 'new project' button

--- project_management_app/src/index.js
+++ project_management_app/src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Container extends React.Component {
+  constructor(props) {
+    super (props);
+    this.state = {
+      //TODO: Add profile photo, username, password
+    };
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  render () {
+    return (
+    <button className = "newProject" onClick={() => {
+      //TODO: implement create new project button
+    }}>
+      New Project
+    </button>
+    );
+  }
+}
+
+function newProject(props) {
+  return (
+    <button className = "newProject" onClick={() => {
+      //TODO: implement create new project button
+    }}>
+      New Project
+    </button>
+  )
+}
+
+class Project extends React.Component {
+  constructor(props) {
+    super (props);
+    this.state = {
+      //deadline: 
+    };
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Container />);
